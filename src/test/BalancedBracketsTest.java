c59cdd7bcb680266e2c8e17c2244a021307fa57c
Jason Wilson <jrwilson89@gmail.com>
2021-09-16 19:41:06 -0500

    Finished Studio

--- src/test/BalancedBracketsTest.java
+++ src/test/BalancedBracketsTest.java
@@ -1,5 +1,6 @@
 package test;
 
+import main.BalancedBrackets;
 import org.junit.Test;
 
 import static org.junit.Assert.*;
@@ -10,6 +11,61 @@ public class BalancedBracketsTest {
     @Test
     public void emptyTest() {
         assertEquals(true, true);
+    }
+
+    @Test
+    public void onlyBracketsReturnsTrue() {
+        assertTrue(BalancedBrackets.hasBalancedBrackets("[]"));
+    }
+
+    @Test
+    public void onlyBracketsReturnsTrue2() {
+        assertTrue(BalancedBrackets.hasBalancedBrackets("[LaunchCode]"));
+    }
+
+    @Test
+    public void onlyBracketsReturnsTrue3() {
+        assertTrue(BalancedBrackets.hasBalancedBrackets("Launch[Code]"));
+    }
+
+    @Test
+    public void onlyBracketsReturnsTrue4() {
+        assertTrue(BalancedBrackets.hasBalancedBrackets("[]LaunchCode"));
+    }
+
+    @Test
+    public void onlyBracketsReturnsTrue5() {
+        assertTrue(BalancedBrackets.hasBalancedBrackets(""));
+    }
+
+    @Test
+    public void onlyBracketsReturnsFalse() {
+        assertFalse(BalancedBrackets.hasBalancedBrackets("[LaunchCode"));
+    }
+
+    @Test
+    public void onlyBracketsReturnsFalse2() {
+        assertFalse(BalancedBrackets.hasBalancedBrackets("Launch]Code["));
+    }
+
+    @Test
+    public void onlyBracketsReturnsFalse3() {
+        assertFalse(BalancedBrackets.hasBalancedBrackets("["));
+    }
+
+    @Test
+    public void onlyBracketsReturnsFalse4() {
+        assertFalse(BalancedBrackets.hasBalancedBrackets("]["));
+    }
+
+    @Test
+    public void onlyBracketsReturnsFalse5() {
+        assertFalse(BalancedBrackets.hasBalancedBrackets("]L["));
+    }
+
+    @Test
+    public void onlyBracketsReturnsTrue6() {
+        assertTrue(BalancedBrackets.hasBalancedBrackets("[Lau]nchCode"));
     }
 
 
